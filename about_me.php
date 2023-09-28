b9c8d7c55a1aaa51317f121c6ce5a0805c5a0e94
Simon <simonjoelwarkentin@proton.me>
2023-09-28 18:12:04 +0000

    made about me in php

---
+++ about_me.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Simon Joel</title>
+    <link id="theme-style" href="style.css" rel="stylesheet">
+    <link id="dark-style" href="dark.css" rel="stylesheet" disabled>
+</head>
+<body>
+    <header>
+        <div id="title-section">
+            <!--img src="logo.png" alt="Logo"-->
+            <h4>About me</h4>
+	<p>
+	This page is my substitute for a social media profile.
+<br><br>
+	I don't use social media anymore, but I still like to share some of my life!
+<br><br>
+	I will keep adding more contnt on here as time goes on.
+	</p>
+        </div>
+        <!--div id="navigation-bar">
+            <ul>
+                <li>
+                    <a href="#content1" class="nav-link" id="nav-link-1">to hire you</a>
+                    <div id="dropdown-1" class="dropdown-content">
+                        <p>Cool! I am available for work!<br>Here you can find:</p>
+                        <a href="cv.html">My CV</a>
+                        <a href="cover_letter.html">Cover Letter</a>
+                        <a href="sorry.html">My Experiences</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#content2" class="nav-link" id="nav-link-2">to see your art</a>
+                    <div id="dropdown-2" class="dropdown-content">
+                        <a href="sorry.html">Paintings</a>
+                        <a href="sorry.html">Photography</a>
+                        <a href="sorry.html">Music</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#content3" class="nav-link" id="nav-link-3">to stalk you</a>
+                    <div id="dropdown-3" class="dropdown-content">
+                        <a href="sorry.html">About Me</a>
+                        <a href="sorry.html">Thoughts</a>
+                        <a href="sorry.html">Photos</a>
+                    </div>
+                </li>
+                <li>
+                    <a href="#content4" class="nav-link" id="nav-link-4">I don't even know</a>
+                    <div id="dropdown-4" class="dropdown-content">
+                        <a href="sorry.html">Self Hosted Something</a>
+                        <a href="sorry.html">Something Else</a>
+                        <a href="sorry.html">Invidious</a>
+                    </div>
+                </li>
+            </ul>
+        </div-->
+    </header>
+    <button id="theme-toggle">
+        <span id="moon-icon">🌙</span>
+        <span id="sun-icon" hidden>☀️</span>
+    </button>
+    <main>
+        <hr>
+        <section id="content2">
+            <h2>Hi, I am Simon Joel</h2>
+	    <p>
+My full name is Simon Joel Warkentin, some close friends call me Joel.
+<br><br>
+Working in sales is my day job, but my heart finds itself in a few other corners of the world too.
+<br>
+My past has led me through the lanes of 5 countries, each teaching me a bit more about life and gifting me with languages I now cherish.
+<br>
+Yet, no matter where I've been, I always see myself as a student, eager to understand and learn.
+<br><br>
+Tech, especially Linux, has been a recent journey for me. I’m far from an expert, but every day brings new challenges and lessons.
+<br>
+And music? It's a passion I've held close. While I certainly do have the look of a metalhead, my musical taste is not at all limited.
+<br>
+Besides Metal, i also listen to (and play) Jazz, classical music, progressive rock and more!
+<br><br>
+Travel and nature have been my silent teachers. They've shown me the beauty in simplicity and the importance of staying grounded.
+<br>
+On the digital front, I’ve been slowly distancing myself from mainstream social media platforms, finding solace in the quieter pockets of the internet.
+<br><br>
+I'm a believer in the power of community and collaboration, which is why I've grown fond of Free and Open Source Software.
+<br>
+There's something special about people coming together to create and share openly.
+<br><br>
+If you ever want to chat about tech, music, or just life in general, don’t hesitate to reach out. My inbox is always open at mail@simonjoel.com.
+<br>
+I'm always grateful for new connections and shared stories.
+
+	    </p>
+	    <!--a href="cv.html">my CV</a>
+		<p></p>
+	    <a href="cover_letter">my Cover Letter</a-->
+        </section>
+        <!--section id="content2">
+            <h2>Art...</h2>
+            <b>...is the most important thing in life, for me.</b>
+            <p>I am a musician since my childhood (used to work in the music industry before covid),<br>and I also enjoy painting and photography.</p>
+            <p>As soon as I have time, I plan on posting some of my art here.</p>
+        </section>
+        <section id="content3">
+            <h2>I like sharing!...</h2>
+            <b>...but I hate social media.</b>
+            <p>This is why I want to share some pictures and thoughts of mine here in this section!
+		</p>
+		<hr>
+		<img src="assets/images/simon2.png" alt="Simon">
+		<p>
+		Hey, I'm Simon Joel Warkentin.
+		<br>My journey has taken me through 5 countries, and along the way, I've picked up 4 languages.
+		<br>Beyond the world of tech and Linux, I often lose myself in creating music or exploring new places.
+		<br>With a deep appreciation for Free and Open Source Software, I'm always learning and evolving.
+		<br>Want a glimpse into my world? Find out more <a href="about_me.html">here</a>.
+            </p>
+        </section>
+        <section id="content4">
+            <h2>Hmmmmm...</h2>
+            <p>Some more content is gonna go here soon.<br>Probably something about self hosting...</p>
+        </section-->
+    </main>
+    <hr>
+    <footer>
+        <a href="https://github.com/SimonJoelWarkentin">This website is an Open Source project by Simon Joel.</a>
+    </footer>
+    <script src="script.js"></script>
+    <script src="theme.js"></script>
+</body>
+</html>
