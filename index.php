6c7b50dd2fafe228241616e1137f2d2d7a3178bd
root <root@new.simonjoel.com>
2023-10-22 10:42:20 +0000

    added self hosted plausible analytics

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <link rel="icon" href="assets/images/logo.png" type="image/icon type">
     <link id="theme-style" href="style.css" rel="stylesheet">
     <link id="dark-style" href="dark.css" rel="stylesheet" disabled>
+<script defer data-domain="simonjoel.com" src="https://stats.simonjoel.com/js/script.js"></script>
 </head>
 <body>
     <header>
